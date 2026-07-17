2d2389303d02ae1059b2d1839c885cb5d792917a
Thomagotchi <thomassano87@gmail.com>
2026-07-17 11:14:04 +0200

    Update - Global - Added global + reset scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import Modal from 'react-modal';
+import './index.scss';
+import App from './App.jsx';
+
+/* Bind react-modal to the app root element for accessibility (aria-hidden on background) */
+Modal.setAppElement('#root');
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
